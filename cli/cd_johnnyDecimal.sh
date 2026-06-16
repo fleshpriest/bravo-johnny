686f0cd39b9258d14ac175185f4155b224e46b62
fleshpriest <fleshpriest@tuta.com>
2026-06-16 12:49:55 -0700

    Added comments to cli script

--- cli/cd_johnnyDecimal.sh
+++ cli/cd_johnnyDecimal.sh
@@ -4,3 +4,6 @@ function jdcd() { cd     "$(jdGetIndexPath.py "${1}")" }
 function jdls() { ls     "$(jdGetIndexPath.py "${1}")" }
 function jdll() { ls -la "$(jdGetIndexPath.py "${1}")" }
 function jdla() { ls -a  "$(jdGetIndexPath.py "${1}")" }
+
+# Example usage:
+# $ jdcd 23   # change directory to area 23
